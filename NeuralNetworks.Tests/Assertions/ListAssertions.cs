37d647f99a3ac3a700ec2c75ecdc2cea3085071a
Iouri Khramtsov <ioukhr@gmail.com>
2015-12-20 21:15:11 -0500

    Completed MultilayerPerceptron.FeedForward.

---
+++ NeuralNetworks.Tests/Assertions/ListAssertions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NeuralNetworks.Tests.Assertions
+{
+    public static class ListAssertions
+    {
+        public static void ShouldApproximatelyEqual(this IList<double> actual, IList<double> expected, double relTolerance)
+        {
+            if (actual == null)
+            {
+                if (expected != null)
+                    throw new Exception("Expected null but got not null");
+
+                return;
+            }
+
+            if (expected == null)
+                throw new Exception("Expected not null but got null.");
+
+            if (actual.Count != expected.Count)
+                throw new Exception($"Expected length {expected.Count} but actual length is {actual.Count}.");
+
+            for (var i = 0; i < expected.Count; i++)
+            {
+                var error = expected[i] == 0.0 ? actual[i] : (actual[i] - expected[i]) / expected[i];
+
+                if (Math.Abs(error) > relTolerance)
+                    throw new Exception($"At index {i}: Expected error to be between than +/-{relTolerance}; was {error}");
+            }
+                
+        }
+    }
+}
--- NeuralNetworks/MultilayerPerceptron.cs
+++ NeuralNetworks/MultilayerPerceptron.cs
@@ -8,8 +8,7 @@ namespace NeuralNetworks
     {
         public int NumInputs { get; }
         public int NumOutputs { get; }
-        public IList<int> HiddenLayerSizes { get; private set; }
-
+        public IList<int> HiddenLayerSizes { get; }
         public double[][] Weights { get; }
 
         public MultilayerPerceptron(int numInputs, int numOutputs, IList<int> hiddenLayerSizes)
@@ -48,17 +47,90 @@ namespace NeuralNetworks
             }
         }
 
+        /// <summary>
+        /// Activation functions: tanh for hidden nodes, softmax for output nodes.
+        /// </summary>
         public FeedForwardResult FeedForward(double[] input)
         {
-            throw new NotImplementedException();
+            if (input.Length != NumInputs)
+                throw new NeuralNetworkException($"Expected input length to be {NumInputs} but got {input.Length}.");
+
+            // Add bias input node (always = 1).
+            var inputsWithBias = new double[NumInputs + 1];
+            Array.Copy(input, inputsWithBias, input.Length);
+            inputsWithBias[inputsWithBias.Length - 1] = 1;
+
+            // Prepare hidden nodes. Include bias node (always = 1) as the last node in each layer.
+            var hidden = new double[HiddenLayerSizes.Count][];
+
+            for (var i = 0; i < HiddenLayerSizes.Count; i++)
+            {
+                var length = HiddenLayerSizes[i] + 1;
+                hidden[i] = new double[length];
+                hidden[i][length - 1] = 1;
+            }
+
+            // Calculate first hidden layer.
+            for (var j = 0; j < HiddenLayerSizes[0]; j++)
+            {
+                var preActivation = 0.0;
+                var offset = j * (NumInputs + 1);
+
+                for (var i = 0; i < NumInputs + 1; i++)
+                    preActivation += inputsWithBias[i] * Weights[0][offset + i];
+
+                hidden[0][j] = Math.Tanh(preActivation);
+            }
+
+            // Calculate the rest of the hidden layers.
+            for (var l = 1; l < HiddenLayerSizes.Count; l++)
+            {
+                for (var j = 0; j < HiddenLayerSizes[l]; j++)
+                {
+                    var preActivation = 0.0;
+                    var prevLayerSize = HiddenLayerSizes[l - 1] + 1;
+                    var offset = j * prevLayerSize;
+
+                    for (var i = 0; i < prevLayerSize; i++)
+                        preActivation += hidden[l-1][i] * Weights[l][offset + i];
+
+                    hidden[l][j] = Math.Tanh(preActivation);
+                }
+            }
+
+            // Calculate the output layer.
+            var lastHiddenLayer = hidden[HiddenLayerSizes.Count - 1];
+            var lastHiddenLayerSize = lastHiddenLayer.Length;
+            var outputWeights = Weights[Weights.Length - 1];
+            var sumOfPreOutputs = 0.0;
+            var preOutputs = new double[NumOutputs];
+
+            for (var k = 0; k < NumOutputs; k++)
+            {
+                var preOutput = 0.0;
+                var offset = k * lastHiddenLayerSize;
+
+                for (var j = 0; j < lastHiddenLayerSize; j++)
+                    preOutput += outputWeights[offset + j] * lastHiddenLayer[j];
+
+                preOutputs[k] = Math.Exp(preOutput);
+                sumOfPreOutputs += preOutputs[k];
+            }
+
+            var outputs = new double[NumOutputs];
+
+            for (var k = 0; k < NumOutputs; k++)
+                outputs[k] = preOutputs[k] / sumOfPreOutputs;
+
+            var result = new FeedForwardResult();
+            result.InputWithBias = inputsWithBias;
+            result.HiddenLayers = hidden;
+            result.Output = outputs;
+
+            return result;
         }
 
         public double[][] CalculateGradients(double[] input, double[] targets)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static void MakeConfig(int numInputs, int numOutputs, IList<int> hiddenLayerSizes)
         {
             throw new NotImplementedException();
         }
